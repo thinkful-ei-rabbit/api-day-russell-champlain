b6cf7d77a8f5ffa62453400996c07a63e10c7479
Devon Reihl <devon.reihl@gmail.com>
2020-08-18 11:14:11 -0700

    step 6 complete

--- src/api.js
+++ src/api.js
@@ -1,23 +1,23 @@
 const BASE_URL = 'https://thinkful-list-api.herokuapp.com/russell';
 
 function getItems() {
-    return fetch(`${BASE_URL}/items`);
-    //return Promise.resolve('A successful response!');
+  return fetch(`${BASE_URL}/items`);
+  //return Promise.resolve('A successful response!');
 }
 
 function createItem(name) {
-    const newItem = JSON.stringify({ name });
+  const newItem = JSON.stringify({ name });
 
-    return fetch(`${BASE_URL}/items`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: newItem
-    });
-};
+  return fetch(`${BASE_URL}/items`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: newItem
+  });
+}
 
 export default {
-    getItems,
-    createItem
+  getItems,
+  createItem
 };
--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import shoppingList from './shopping-list';
 
 const main = function () {
   api.getItems()
-  .then(res => res.json())
-  .then((items) => {
-    items.forEach((item) => store.addItem(item));
-    shoppingList.render();
-  });
+    .then(res => res.json())
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
